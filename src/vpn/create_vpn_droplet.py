7f73c46738038ed52bd1e829d1c8439f0b8c643d
Timur Milovanov <milovtim@ya.ru>
2019-11-07 10:27:02 +0300

    Write droplet IP to file

--- src/vpn/create_vpn_droplet.py
+++ src/vpn/create_vpn_droplet.py
@@ -3,4 +3,7 @@ from vpn import actions
 if __name__ == '__main__':
     d = actions.create_vpn_droplet()
     actions.wait_droplet_state(d.id, 'create', 'completed')
-    print("Check ip: %s" % d.load().ip_address)
+    ip_addr = d.load().ip_address
+    with open('vpn_ip.txt', 'w') as ip_file:
+        ip_file.write(ip_addr)
+    print("Check ip: %s" % ip_addr)
